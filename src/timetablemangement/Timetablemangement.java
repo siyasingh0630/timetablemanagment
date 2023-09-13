0afd1f2dfe81efacefe139ce963d9c41d137049a
siyasingh0630 <99880932+siyasingh0630@users.noreply.github.com>
2023-09-13 16:24:25 +0530

    Update Timetablemangement.java

--- src/timetablemangement/Timetablemangement.java
+++ src/timetablemangement/Timetablemangement.java
@@ -15,7 +15,7 @@ public class Timetablemangement {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Hello Everyone
+        //Siya here
     }
     
 }
